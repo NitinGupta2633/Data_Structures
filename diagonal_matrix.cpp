cd9ef2e3603aeb6f5023b3ac00e3f83609d7fdf6
NitinGupta2633 <62183640+NitinGupta2633@users.noreply.github.com>
2020-06-27 12:23:55 +0530

    defining and printing a diagonal matrix

---
+++ diagonal_matrix.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+int print_diagonal(int arr[],int n);
+int main()
+{
+	int n;
+	std::cout<<"enter the size of square matrix "<<std::endl;
+	std::cin>>n;
+	int arr[n];
+	std::cout<<"enter all the digonal element"<<std::endl;
+	for(int i=0;i<n;i++)
+	{
+		std::cout<<"enter the element "<<i+1<<std::endl;
+		std::cin>>arr[i];
+	}
+	print_diagonal(arr,n);
+}
+int print_diagonal(int arr[],int n)
+{
+	int arr1[n][n];
+	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			arr1[i][j]=0;
+			if(i==j)
+			{
+				arr1[i][j]=arr[i];
+			}
+			std::cout<<arr1[i][j]<<" ";
+			
+		}
+		std::cout<<"\n";
+	}
+}
